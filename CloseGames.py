de02294e4b96e440406c5227afe9ea05d2a825cc
XingxingSun <wysglby@gmail.com>
2017-09-23 19:57:07 -0400

    timeadded

--- CloseGames.py
+++ CloseGames.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from Queue import PriorityQueue
+from operator import itemgetter
 import csv
 
 # for this file, we want to find games whose score difference between 2 teams are <-2 or >20
@@ -5,7 +8,8 @@ import csv
 def closeGames(filename):
     games = {'gameID': 'player'}
     # this dictionary stores for last row
-    lastRow = {"id": "lastRowDiff"}
+    lastRow = {'id': '(lastRowDiff,startTime'}
+    # time the "run" starts
     with open(filename, 'rU') as f:
         reader = csv.reader(f, delimiter=",")
         next(reader)
@@ -24,13 +28,15 @@ def closeGames(filename):
             player8 = row[37]
             player9 = row[38]
             player10 = row[39]
-            lastRow[game_id] = int(homePts) - int(visitorPts)
+            time = row[14]
+            # we don't need to record end time because it's always 0
+            lastRow[game_id] = (int(homePts) - int(visitorPts))
             if (int(homePts) - int(visitorPts)
-                    == 10 or int(homePts) - int(visitorPts) == -10) and int(period) == 4 and lastRow[game_id] <= 2:
+                    == 10 or int(homePts) - int(visitorPts) == -10) and int(period) == 4 and lastRow[game_id][0] <= 2:
                 if not game_id in games.keys():
                     games[game_id] = [player1, player2, player3,
-                                    player4, player5, player6, player7, player8, player9, player10]
-        print games
+                                    player4, player5, player6, player7, player8, player9, player10, time]
+    return [games, lastRow]
 
 def countFactors(gameDic, filename):
     with open(filename, 'rU') as f:
@@ -63,3 +69,9 @@ def countFactors(gameDic, filename):
 
 if __name__ == '__main__':
     games = closeGames('Play_by_Play_New.csv')
+    print games
+    # print games.__len__()
+
+
+
+
